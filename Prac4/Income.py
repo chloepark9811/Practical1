52ffb080609bee49d3437aa2183c21d9921ed870
xnote <chaeyeon.park@my.jcu.edu.au>
2018-11-27 11:09:55 +0800

    yeet

--- Prac4/Income.py
+++ Prac4/Income.py
@@ -1,23 +1,32 @@
 """
-CP1404/CP5632 Practical
-Starter code for cumulative total income program
+CP1404/CP5632 Practical - Suggested Solution
+Cumulative total income program
 """
 
 
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    incomemonths = int(input("How many months? "))
+    number_of_months = int(input("How many months? "))
 
-    for month in range(1, incomemonths + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, number_of_months + 1):
+        income = float(
+            input("Enter income for month {}: ".format(month)))
         incomes.append(income)
 
+    print_report(incomes)
+
+
+def print_report(incomes):
+    """Print report based on incomes."""
+    # Note that we do not need to pass in number_of_months
+    # because we know the length of the incomes list
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, incomemonths + 1):
-        income = incomes[month - 1]
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+    for month, income in enumerate(incomes):
+        total += income
+        print("Month {:2} - Income: ${:10.2f} \
+        Total: ${:10.2f}".format(month + 1, income, total))
 
 
 main()

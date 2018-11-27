a700eede367ab0d6b18e73b6aab4d6e714459c6b
xnote <chaeyeon.park@my.jcu.edu.au>
2018-11-27 11:12:56 +0800

    yeet

--- Prac4/Income.py
+++ Prac4/Income.py
@@ -1,9 +1,3 @@
-"""
-CP1404/CP5632 Practical - Suggested Solution
-Cumulative total income program
-"""
-
-
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
